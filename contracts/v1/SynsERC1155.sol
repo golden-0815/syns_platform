3ba4e2c92da540304e6b8efa49a841cad09b7725
golden-0815 <andreas.larsen.062410@gmail.com>
2026-04-30 01:02:56 +0900

    chore: added erc721 and erc1155 files

---
+++ contracts/v1/SynsERC1155.sol
@@ -0,0 +1,144 @@
+/*
+    @dev: Logan (Nam) Nguyen
+    @Course: SUNY Oswego - CSC 495 - Capstone
+    @Instructor: Professor Bastian Tenbergen
+    @Version: 1.0
+    @Hornor: Thirdweb & Openzeppeline
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+/** EXTERNAL IMPORT */
+import "../../libs/thirdweb/ERC1155base.sol";
+import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
+
+/**
+ *  The `SynsERC1155` smart contract implements the Thirdweb/ERC1155Base NFT standard.
+ *  It includes all the standard logic from ERC1155 PLUS:
+ *      - Emit event newTokenMintedTo (if a new token is minted) everytime mintTo() is called
+ *      - Emit event mintedOnExistedToken (if more supply is added to an existed token) everytime mintTo() is called
+ *      - Records the original creator of the NFT when a new token is created by adding the original creator's address to a mapping
+ */
+contract SynsERC1155 is ERC1155Base, PermissionsEnumerable {
+    /*//////////////////////////////////////////////////////////////
+                        Variables
+    //////////////////////////////////////////////////////////////*/
+    // Mapping(s)
+    mapping (uint256 => address) private tokenIdToOriginalCreator;
+
+    // Mapping tokenID => timestamp
+    mapping (uint256 => uint256) private tokenIdToTimestamp;
+
+    // Event(s)
+    event newTokenMintedTo(address indexed to, uint256 indexed tokenId, string uri, uint256 amount, uint256 indexed royaltyBps);
+    event mintedOnExistedToken(address indexed to, uint256 indexed tokenId, string uri, uint256 amount, uint256 indexed royaltyBps);
+
+
+    /*//////////////////////////////////////////////////////////////
+                        Constructor
+    //////////////////////////////////////////////////////////////*/
+    constructor() ERC1155Base("Spark Your Noble Story", "SYNS1155"){
+        // grant admin role to deployer
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+
+    /*//////////////////////////////////////////////////////////////
+                        SynsERC1155v1 Logic
+    //////////////////////////////////////////////////////////////*/
+    
+    /**
+     *  @dev             - If `_tokenId == getNewTokenRequiredId()` 
+     *                          + A new NFT at tokenId `nextTokenIdToMint` is minted.
+     *                          + Set the royalty recipient to the address msg.sender
+     *                          + Emits event newTokenMintedTo().
+     *                   - If the given `tokenId < nextTokenIdToMint`, then additional supply of an existing NFT is being minted
+     *                      on existed token at _tokenId, and the tokenURI is set to be the same. 
+     *                      Emits event mintedOnExistedToken().
+     *
+     *  @notice          Noted removed onlyRole(DEFAULT_ADMIN_ROLE) => the logic is to let every one mint NFTs not just Syns Service Admin account
+     *
+     *  @param _tokenId  The tokenId of the NFT to mint.
+     *  @param _tokenURI The full metadata URI for the NFTs minted (if a new NFT is being minted).
+     *  @param _amount   The amount of the same NFT to mint.
+     */
+    function safeMintTo(
+        uint256 _tokenId, 
+        string memory _tokenURI, 
+        uint256 _amount,
+        uint256 _royaltyBps
+    ) public {
+        // newTokenRequiredId is the ID that _tokenId must meet to create a new token. Otherwise, more supply are minted on existed tokens.
+        uint newTokenRequiredId = getNewTokenRequiredId();
+
+        // if _tokenId != newTokenRequiredId => msg.sender is required to be the originalAuthor
+        if (_tokenId != newTokenRequiredId) {
+            require(msg.sender == tokenIdToOriginalCreator[_tokenId], "!AUTHOR - only original author can add more supply to this token");
+        }
+
+        // nextTokenIdToMint is the new tokenId of the new token being created.
+        uint nextTokenIdToMint = super.nextTokenIdToMint();
+
+        // Calls mintTo() from ERC1155Base
+        super.mintTo(msg.sender, _tokenId, _tokenURI, _amount);
+
+
+        // originalCreator logic
+        if (_tokenId == newTokenRequiredId) { // new token is being created
+            tokenIdToOriginalCreator[nextTokenIdToMint] = msg.sender;
+            _setupRoyaltyInfoForToken(nextTokenIdToMint, msg.sender, _royaltyBps);
+            
+            // update tokenIdToTimestamp mapping
+            tokenIdToTimestamp[nextTokenIdToMint] = block.timestamp;
+
+            // emit newTokenMintedTo event
+            emit newTokenMintedTo(msg.sender, nextTokenIdToMint, _tokenURI, _amount, _royaltyBps);
+        } else { // more supplies are being minted on an existed token
+            emit mintedOnExistedToken(msg.sender, nextTokenIdToMint, _tokenURI, _amount, _royaltyBps);
+        }
+    }
+
+
+    /*//////////////////////////////////////////////////////////////
+                        SynsERC1155v1 Getters
+    //////////////////////////////////////////////////////////////*/
+
+     /// @dev Returns a slice of tokens that are owned by a wallet address
+     /// @notice Needs to find another approach in future version - well, or maybe not too bad
+     /// @notice Client app can call this api to get the list of ids => then call ERC.blanceof to find out how many copies the owner has per each tokenId
+    function getTokensOwnedBy(address owner) view public returns (int256[] memory) {
+
+        // prepare total amount of tokens
+        uint256 totalTokens = ERC1155Base.nextTokenIdToMint();
+
+        // // init tokens array
+        int256[] memory tokens = new int256[](totalTokens);
+
+        // loops through smart contracts if a token is owned by `owner` => push to `tokens` else uint256.max
+        for (uint256 i = 0; i < totalTokens; i++) {
+            if (ERC1155.balanceOf[owner][i] != 0) {
+                tokens[i] = int256(i);
+            } else {
+                tokens[i] = -1;
+            }
+        }
+
+        return tokens;
+    }
+
+    /// @dev Returns the biggest uint256 value to set a bar for creating a new token
+    function getNewTokenRequiredId() pure public returns (uint256) {
+        return type(uint256).max;
+    }
+
+    /// @dev Returns originalCreator by tokenId
+    function getOriginalCreator(uint _tokenId) view public returns (address) {
+        return tokenIdToOriginalCreator[_tokenId];
+    }
+
+    /// @dev Returns the block timestamp when the token is minted 
+    function getTokenTimestamp(uint256 _tokenId) view public returns (uint256) {
+        return tokenIdToTimestamp[_tokenId];
+    }
+}
---
+++ contracts/v1/SynsERC721.sol
@@ -0,0 +1,173 @@
+/*
+    @dev: Logan (Nam) Nguyen
+    @Course: SUNY Oswego - CSC 495 - Capstone
+    @Instructor: Professor Bastian Tenbergen
+    @Version: 1.0
+    @Hornor: Thirdweb & Openzeppeline
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+/** EXTERNAL IMPORT */
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
+/**
+ *  The `SynsERC721` smart contract implements the Openzeppelin/ERC721 NFT standard, along with the ERC721Royalty optimization.
+ *  It includes all the standard logics from ERC721A & ERC721Base PLUS:
+ *      - Emit event mintedTo() everytime mintTo() is called
+ *      - Records the original creator of the NFT by adding the original creator's address to a mapping
+ */
+contract SynsERC721 is ERC721URIStorage, ERC721Royalty, AccessControl {
+    /*//////////////////////////////////////////////////////////////
+                        State variables
+    //////////////////////////////////////////////////////////////*/
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    /*//////////////////////////////////////////////////////////////
+                        Structs
+    //////////////////////////////////////////////////////////////*/
+    struct RoyaltyInfoForAddress {
+        address originalAuthor;
+        uint96 royaltyBPS;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        Mappings
+    //////////////////////////////////////////////////////////////*/
+    // Mapping tokenID => originalCreator => royaltyBPS
+    mapping (uint256 => RoyaltyInfoForAddress) private royaltyInfoForToken;
+
+    // Mapping tokenID => timestamp
+    mapping (uint256 => uint256) private tokenIdToTimestamp;
+
+    mapping(uint256 => uint256[]) private tokensOwnedBy;
+
+    /*//////////////////////////////////////////////////////////////
+                        Events
+    //////////////////////////////////////////////////////////////*/
+    event newTokenMintedTo(address indexed to, uint256 indexed tokenId, string uri, uint256 indexed royaltyBps);
+
+    /*//////////////////////////////////////////////////////////////
+                        Constructor
+    //////////////////////////////////////////////////////////////*/
+    constructor() ERC721("Spark Your Noble Story", "SYNS721") {
+        // grant admin role to deployer
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        SynsERC721v1 Logic
+    //////////////////////////////////////////////////////////////*/
+
+     /**
+     *  @notice             Lets any addresses mint an NFT to themselves or to another recepient. Override @erc721a.mintTo()
+     *  @dev                After finished minting new token, _setupRoyaltyInfoForToken() is called to set the originalCreator as
+     *                      royalty recipient address
+     *
+     *  @param _tokenURI    The token uri
+     *
+     *  @param _royaltyBps  The bps for royalty
+     */
+     function safeMintTo(string memory _tokenURI, uint96 _royaltyBps) public returns (uint) {
+        // prepare nextTokenIdToMint
+        uint256 nextIdToMint = _tokenIds.current();
+
+        // mint a new token
+        _safeMint(msg.sender, nextIdToMint);
+        _setTokenURI(nextIdToMint, _tokenURI);
+
+        // update RoyaltyInfoForToken mapping
+        RoyaltyInfoForAddress memory currentRoyaltyInfo = RoyaltyInfoForAddress({
+            originalAuthor: msg.sender,
+            royaltyBPS: _royaltyBps}
+        );
+        royaltyInfoForToken[nextIdToMint] = currentRoyaltyInfo;
+
+        // set roytalty recipient for token
+        _setTokenRoyalty(nextIdToMint, msg.sender, _royaltyBps);
+
+        // update tokenIdToTimestamp mapping
+        tokenIdToTimestamp[nextIdToMint] = block.timestamp;
+
+        // emit event
+        emit newTokenMintedTo(msg.sender, _tokenIds.current(), _tokenURI, _royaltyBps);
+
+        // increment tokenId
+        _tokenIds.increment();
+
+
+        return nextIdToMint;
+    }
+
+
+    // The following functions are overrides required by Solidity.
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    
+    function _burn(uint256 tokenId) internal override(ERC721URIStorage, ERC721Royalty) {
+        super._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Royalty, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        SynsERC721v1 Getters
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Returns a slice of tokens that are owned by a wallet address
+    function getTokensOwnedBy(address owner) view public returns (int256[] memory) {
+
+        // prepare total amount of tokens
+        uint256 totalTokens = _tokenIds.current();
+
+        // // init tokens array
+        int256[] memory tokens = new int256[](totalTokens);
+
+        // loops through smart contracts if a token is owned by `owner` => push to `tokens` else uint256.max
+        for (uint256 i = 0; i < totalTokens; i++) {
+            if (ERC721.ownerOf(i) == owner) {
+                tokens[i] = int256(i);
+            } else {
+                tokens[i] = -1;
+            }
+        }
+
+        return tokens;
+    }
+
+    /// @dev Returns originalCreator by tokenId
+    function getRoyaltyInfoForToken(uint _tokenId) view public returns (RoyaltyInfoForAddress memory) {
+        return royaltyInfoForToken[_tokenId];
+    }
+
+    /// @dev Returns the total amount of tokens created on this contract
+    function nextTokenIdToMint() view public returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    /// @dev Returns the block timestamp when the token is minted 
+    function getTokenTimestamp(uint256 _tokenId) view public returns (uint256) {
+        return tokenIdToTimestamp[_tokenId];
+    }
+}
