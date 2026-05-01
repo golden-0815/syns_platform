e796e1741885e62cf0e71b57dbcbdf09bd6e7c1a
golden-0815 <andreas.larsen.062410@gmail.com>
2026-05-02 05:25:49 +0900

    chore: added club and donating file

---
+++ utils/constants.ts
@@ -0,0 +1,26 @@
+/*
+    @dev: Logan (Nam) Nguyen
+    @Course: SUNY Oswego - CSC 495 - Capstone
+    @Instructor: Professor Bastian Tenbergen
+    @Version: 1.0
+*/
+
+/** @notice native token wrapper */
+export const NAVTIVE_TOKEN_WRAPPER =
+  '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+
+/** @notice Name for Syns NFT smart contract  */
+export const SYNS_NFT_NAME = 'Support-Who-You-Love';
+
+/** @notice Symbol for Syns NFT smart contract  */
+export const SYNS_NFT_SYMBOL = 'SYNS';
+
+/** @notice Default royalty recipient for Syns NFT smart contract  */
+export const SYNS_NFT_SERVICE_RECIPIENT =
+  '0x0851072d7bB726305032Eff23CB8fd22eB74c85B';
+
+/** @notice Default Royalty BPS for Syns NFT smart contract  */
+export const SYNS_NFT_DEFAULT_ROYALTY_BPS = '1000';
+
+/** @notice Default Platform  for Syns NFT smart contract  */
+export const SYNS_NFT_DEFAULT_PLATOFRM_FREE_BPS = '2000';
